5a1e0d47a122151f0295ba4fa69dd15d68677327
Luiz Felipe <40125808+luizfelipedasilva678@users.noreply.github.com>
2021-11-26 22:44:47 -0300

    Update list_1_1.sql

--- list_1_1.sql
+++ list_1_1.sql
@@ -1,6 +1,6 @@
 SELECT descricao, endereco FROM fornecedores WHERE estado = 'SP' AND (cidade IN('DIADEMA','BARUERI'));
 SELECT * FROM fornecedores WHERE estado = 'SP' and (cidade != 'BROTAS' AND cidade != 'SAO PAULO');
 SELECT descricao, telefone FROM fornecedores WHERE (estado IN ('MG','SP')) AND (cidade != '');
-SELECT * FROM fornecedores ORDER BY descricao LIMIT 5,8;
+SELECT * FROM fornecedores ORDER BY descricao LIMIT 4,8;
 SELECT descricao, pcusto, pcusto * 1.3 AS preco_custo_aumentado FROM produtos WHERE estoque > 0;
 SELECT descricao, pcusto, estoque, pcusto * 1.25 AS balanco from produtos;
